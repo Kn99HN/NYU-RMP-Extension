b9235ecb04cdce8ac72c3aec2c6e00fb447fbafe
khanhnguyen <khanhnguyen99hn@gmail.com>
2019-09-07 18:56:32 -0400

    commits

--- background.js
+++ background.js
@@ -26,6 +26,9 @@ function myAlert() {
   });
 }
 
+/*
+Send the link to the content scripts
+*/
 function sendLink() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
@@ -40,28 +43,24 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "open_new_tab" ) {
       var list = request.url;
-      //Create the container after the button is clicked
       createPane();
       var link = [];
       //Iterate through the list of instructor and get links for each
       for(var i = 0; i < list.length; i++) {
-        searchProf(list[i], link, function(value, name) {
-            //alert('link size is ' + link.length);
-            //Create the element anchor with the link and target blank
-            var professor = document.createElement('a');
-            professor.setAttribute('href',value);
-            professor.setAttribute('target',"_blank");
-            professor.innerHTML = name;
-            //sendLink(value, name);
-            var container = document.getElementById('Professor');
-            if(container != null) {
-              container.appendChild(professor);
-            }
-            chrome.storage.local.set({'List': link});
-            //return link;
-        });
+        var className = list[i]["Class"];
+        searchProf(list[i]["Professor"], link, function(name, value) {
+           //Create the element anchor with the link and target blank
+           var professor = document.createElement('a');
+           professor.setAttribute('href',value);
+           professor.setAttribute('target',"_blank");
+           professor.innerHTML = name + ' - ' + className;
+           var container = document.getElementById('Professor');
+           if(container != null) {
+             container.appendChild(professor);
+           }
+       });
       }
-      sendLink();
+      //sendLink();
     }
     //Hide the button after clicking
     hideButton();
@@ -113,40 +112,42 @@ Search Professor Name using XMLHttpRequest and check if their query has New York
 If so, append a link.
 */
 function searchProf(name, list, callback) {
-  var names = name.toString().split(" ");
-  var firstName = names[0];
-  var lastName = names[1];
-  var req = new XMLHttpRequest();
-  var param = '';
-  for(var i =  0; i < names.length - 1; i++) {
-    param += names[i] + '+';
-  }
-  param += names[names.length - 1];
-  //Make a XMLHttpRequest to the server using all the available name
-  req.open("GET", 'https://www.ratemyprofessors.com/search.jsp?query=' + param, true);
-  req.onreadystatechange = function() {
-        if (req.readyState == 4 ) {
-          if(req.status == 200) {
-            var text = req.responseText;
-            //If the text has New York University or NYU, include it and extract
-            if(text.includes('New York University') === true || text.includes('NYU')) {
-              var dummy = document.createElement('html');
-              dummy.innerHTML = text;
-              professors = dummy.getElementsByClassName('listing PROFESSOR');
-              //return the links of the associated professor
-              for(var i = 0; i < professors.length; i++) {
-                var school = professors[i].getElementsByClassName('sub');
-                if(school[0].textContent.includes('New York University')
-                  || school[0].textContent.includes('NYU')) {
-                  link = 'https://www.ratemyprofessors.com' + professors[i].innerHTML.match(/href="([^"]*)/)[1];
+  if(name != undefined) {
+      var names = name.toString().split(" ");
+      var firstName = names[0];
+      var lastName = names[1];
+      var req = new XMLHttpRequest();
+      var param = '';
+      for(var i =  0; i < names.length - 1; i++) {
+        param += names[i] + '+';
+      }
+      param += names[names.length - 1];
+      //Make a XMLHttpRequest to the server using all the available name
+      req.open("GET", 'https://www.ratemyprofessors.com/search.jsp?query=' + param, true);
+      req.onreadystatechange = function() {
+            if (req.readyState == 4 ) {
+              if(req.status == 200) {
+                var text = req.responseText;
+                //If the text has New York University or NYU, include it and extract
+                if(text.includes('New York University') === true || text.includes('NYU')) {
+                  var dummy = document.createElement('html');
+                  dummy.innerHTML = text;
+                  professors = dummy.getElementsByClassName('listing PROFESSOR');
+                  //return the links of the associated professor
+                  for(var i = 0; i < professors.length; i++) {
+                    var school = professors[i].getElementsByClassName('sub');
+                    if(school[0].textContent.includes('New York University')
+                      || school[0].textContent.includes('NYU')) {
+                      link = 'https://www.ratemyprofessors.com' + professors[i].innerHTML.match(/href="([^"]*)/)[1];
+                    }
+                  }
+                  callback(name,link);
                 }
               }
-              list.push({"Prof_name": name, "link": link});
-              callback(link,name);
             }
-          }
-        }
-    };
-    req.send();
+        };
+        req.send();
+  }
+
 
 }
